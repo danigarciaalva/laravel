a9b81109fcfc5c363a811ae36af6cbad418306a1
Daniel García Alvarado <poodle@poodle-developer-PC.(none)>
2013-05-11 00:51:58 -0500

    Fisrt Commit

---
+++ application/views/home/index.blade.php
@@ -0,0 +1,5 @@
+@layout('master')
+
+@section('content')
+	Daniel :3 
+@endsection
---
+++ application/views/master.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Android</title>
+	<meta name="viewport" content="width=device-width">
+</head>
+<body>
+	<div class="container">
+		@yield('content')
+	</div>
+</body>
+</html>
